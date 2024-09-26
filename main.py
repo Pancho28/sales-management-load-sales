5f9f443cfeee0928141444c5be1d83c9cacd65f8
Francisco Bravo <francisco.bravoleiva@externos-cl.cencosud.com>
2024-09-26 16:23:05 -0300

    Se quito la fecha de creacion para el drop de df de ventas

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
             if dfSalesPaid.shape[0] > 0:
                 logger.info(f'Total sales paid {dfSalesPaid.venta.nunique()}')
                 dfSales = pd.concat([dfSales, dfSalesPaid], ignore_index=True)
-            dfSales = dfSales.drop_duplicates(subset=['venta', 'totalDl', 'totalBs', 'fechacreacion', 'producto', 'categoria', 'precio', 'cantidad', 'fechaentrega'])
+            dfSales = dfSales.drop_duplicates(subset=['venta', 'totalDl', 'totalBs', 'producto', 'categoria', 'precio', 'cantidad', 'fechaentrega'])
             logger.info(f'Total sales {dfSales.venta.nunique()}')
             logger.info(f'Total sales records {dfSales.shape[0]}')
             # Flujo de pagos
