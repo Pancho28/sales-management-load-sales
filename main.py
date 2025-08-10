3caad7c7a6768244449d6c9328241968da9d1549
Francisco Bravo <francisco.bravoleiva@externos-cl.cencosud.com>
2025-08-10 15:37:56 -0400

    Se modifico handler de loguru para separar logs de info y warning

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ from config.db import DBConnection
 from config.alchemy import AlchemyConnection
 from helper.enum import dias_semana
 
+# Elimina el handler por defecto que envía todo a stderr
+logger.remove()
+
+# Configura un handler para los logs de nivel INFO y DEBUG
+# Se envía a stdout y se filtra para que no incluya los logs de nivel WARNING o superior.
+logger.add(sys.stdout,
+           level="INFO",
+           # Usa logger.level para acceder al número de nivel
+           filter=lambda record: record["level"].no < logger.level("WARNING").no)
+
+# Configura un segundo handler para los logs de nivel WARNING, ERROR y CRITICAL
+# Se envía a stderr.
+logger.add(sys.stderr,
+           level="WARNING")
+
 def main():
     load_dotenv()
     try:
